dd10a6ed70081da913b8070b140980e55373ee36
sabyrkalys <sabyralievkalys@gmail.com>
2024-12-15 22:57:37 +1200

    first_commit

---
+++ script.js
@@ -0,0 +1,89 @@
+// типы данных
+// Number 
+// 1,32,
+// 23,5
+// 34,9
+// String
+// '12'
+// 'true'
+// 'Николай'
+// Boolean
+
+// typeof('js')
+
+// null
+// undefined
+// BigInt
+// Symbol
+
+// Object
+// let obj = {
+
+// }
+// Array
+
+// объявление переменных
+// let name = 12;
+// let name2 = 'js'
+// console.log('name2', name2)
+// let i, y, n;
+// i = 45;
+// console.log(name);
+
+// математические операции
+// + - * / ** %
+
+// < > >= <= != == ===
+
+// and or || &&
+
+// let num
+// var num2
+// let cinema1 = '';
+// let cinema2 = '';
+// let cinema3 = '';
+
+// alert('Привет мир!')
+
+// cinema1 = prompt('Укажи свой возраст');
+// console.log('cinema1', cinema1)
+// let num = 10
+// if (num < 20) {
+//     let a =num + 1;
+//     console.log('a', a)
+// } else {
+//     console.log('число больше 20')
+// }
+
+// if(условие) {
+
+// } else if (условие) {
+
+// } else if (условие) {
+
+// } else {
+
+// }
+// Услованые операторы
+let age = 3;
+let num = prompt('угадй возраст от 1 до 10');
+if(num > 3) alert('Число меньше')
+
+// Тернарные операторы
+let a = num > age ? alert('Число меньше') : alert('Число больше');
+
+// switch (имя переменной ) {
+//     case вариант_1:
+//     console.log('случай_1')
+//     break
+//     case вариант_2
+//     console.log('случай_2')
+    
+//     default:
+//     console.log('случай по умолчанию')
+//     break
+// }
+
+// 1.Объявить переменную с помощью let, указать в переменную значение prompt "Напиши в поле друг";
+// 2.Созать условие if(let = 'друг')
+
